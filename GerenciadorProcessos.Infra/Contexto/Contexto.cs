5bb599867505c669cc03c3ef903835ba1a6e031a
Clayton <claytonmenezes@hotmail.com>
2020-01-27 22:00:21 -0300

    iniciando as configuracoes

---
+++ GerenciadorProcessos.Infra/Contexto/Contexto.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace GerenciadorProcessos.Infra.Contexto
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base("GerenciadorProcessos") { }
+    }
+}
---
+++ GerenciadorProcessos.Infra/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace GerenciadorProcessos.Infra.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<GerenciadorProcessos.Infra.Contexto.Contexto>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Contexto.Contexto context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}

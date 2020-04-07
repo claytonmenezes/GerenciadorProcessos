e75e91dac61ad6fe4fb3510920983f647098d044
Clayton <claytonmenezes@hotmail.com>
2020-04-07 00:14:25 -0300

    fake

---
+++ GerenciadorProcessos.Infra/Migrations/202004070312167_fake.cs
@@ -0,0 +1,16 @@
+﻿namespace GerenciadorProcessos.Infra.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fake : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
